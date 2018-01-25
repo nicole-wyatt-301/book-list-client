b4d31e90281cbd183ac9295a1ee10799f0d9ce0d
nicoleweese <nweese@ymail.com>
2018-01-24 20:52:28 -0800

    fixed errors, books render to page now

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -5,15 +5,18 @@ var app = app || {};
 
 
 (function (module) {
+
+  // This is called from book.js and is the callback function that is performed once 
   bookView.initIndexPage = () => {
-    $('.container').hide();
-    $('.book-view').show();
-
     app.Book.all.forEach(a => $('#book-list').append(a.toHtml()));
-
+    
+    $('.book-container').show();
   };
 
+
   module.bookView = bookView;
-  $(document).ready(app.Book.fetchAll(bookView.initIndexPage));
+
+  // don't think we need this anymore because we're now calling it in routes.js but i may be wrong???
+  // $(document).ready(app.Book.fetchAll(bookView.initIndexPage));
 
 })(app)
